d5228754783b9419843d3e90eed3a3edd131a02a
Nishant shukla <shuklanishant85@gmail.com>
2024-11-18 21:42:22 +0530

    Adding Data Model, closes #2

---
+++ src/main/java/com/genesis/beer/web/model/BeerDTO.java
@@ -0,0 +1,25 @@
+package com.genesis.beer.web.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.math.BigDecimal;
+import java.time.OffsetDateTime;
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class BeerDTO {
+    private UUID id;
+    private Integer version;
+    private OffsetDateTime createdDt;
+    private OffsetDateTime lastModifiedDt;
+    private String beerName;
+    private BeerStyleEnum beerStyle;
+    private Long upc;
+    private BigDecimal price;
+    private Integer quantityOnHand;
+}
---
+++ src/main/java/com/genesis/beer/web/model/BeerPageList.java
@@ -0,0 +1,17 @@
+package com.genesis.beer.web.model;
+
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+
+public class BeerPageList extends PageImpl<BeerDTO> {
+
+    public BeerPageList(List<BeerDTO> content, Pageable pageable, long total) {
+        super(content, pageable, total);
+    }
+
+    public BeerPageList(List<BeerDTO> content) {
+        super(content);
+    }
+}
